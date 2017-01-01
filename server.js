3cb9c49cc57cf50da7c28f4e29a08289e32f7373
Corey Slaven <coreyslaven@aol.com>
2016-12-31 21:10:06 -0500

    Server functionality added

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let bodyParser = require('body-parser');
+let moment = require('moment');
+let app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use((req, res) => {
+  let parameter = decodeURIComponent(req.path);
+  let naturalDate = moment(parameter).format("MM-DD-YYYY");
+  let unixDate = moment(parameter).unix();
+
+  let dateObject = {
+    unix: unixDate,
+    natural: naturalDate
+  };
+
+
+  res.send(dateObject);
 })
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app is listening on port 3000');
 })
